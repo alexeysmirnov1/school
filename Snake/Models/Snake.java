228e15e4ab79a0fad09323ee1ad9d8e546e23e1f
TheShiro <smirnovsart@gmail.com>
2023-01-15 00:29:21 +0500

    snake can grow

--- Snake/Models/Snake.java
+++ Snake/Models/Snake.java
@@ -12,19 +12,23 @@ public class Snake implements Model {
     private static ArrayList<int[]> tail = new ArrayList<int[]>();
 
     public void setPosition(int x, int y) {
+        this.moveTail(Snake.xHeadPosition , Snake.yHeadPosition);
+
         Snake.xHeadPosition = x;
         Snake.yHeadPosition = y;
 
-        this.moveTail();
-
         Snake.grow = false;
-
-        System.out.println(tail.size());
     }
 
-    public void moveTail() {
+    public void moveTail(int x, int y) {
+        int[] prev = {x, y};
         if (!Snake.grow) {
-            //
+            for (int[] part : Snake.tail) {
+                int[] current = {part[0], part[1]};
+                part[0] = prev[0];
+                part[1] = prev[1];
+                prev = current;
+            }
         }
     }
 
