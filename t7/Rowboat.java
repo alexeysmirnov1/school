2249cffdc5211153832cd480d28b2b46377e576b
TheShiro <smirnovsart@gmail.com>
2023-01-16 02:06:26 +0500

    tema7

---
+++ t7/Rowboat.java
@@ -0,0 +1,7 @@
+package t7;
+
+public class Rowboat extends Boat {
+    public void rowTheBoat() {
+        System.out.println("Давай,Наташа!");
+    }
+}
---
+++ t7/Sailboat.java
@@ -0,0 +1,7 @@
+package t7;
+
+public class Sailboat extends Boat {
+    public void move() {
+        System.out.println("Поднять паруса!");
+    }
+}
